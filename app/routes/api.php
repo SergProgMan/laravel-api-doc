b4a44bbcf01b1771c286ddd420e46429128eb233
SergProgMan <semibratyev@gmail.com>
2019-02-21 11:40:19 +0200

    made routes

--- app/routes/api.php
+++ app/routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('/user', 'UserController', [
+    'except' => ['edit', 'show', 'create']
+  ]);
+
+  Route::resource('/company', 'CompanyController', [
+    'except' => ['edit', 'show', 'create']
+  ]);
